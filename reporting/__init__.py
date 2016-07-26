a9db0f58cb2afb26de4f4b2a0bbf87ccd5360059
Joe Rieger <jmrieger@mtu.edu>
2016-07-26 16:39:06 -0400

    Modify base template

--- reporting/__init__.py
+++ reporting/__init__.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('chart.html');
+	return render_template('charts.html')
+
+@app.route('/charts/')
+def charts():
+    return render_template('charts.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
